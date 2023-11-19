9616a39212a0e603197bd70bbaaceda55d6a4c97
dumia16 <dujay77@gmail.com>
2023-11-18 16:31:09 -0800

    prepare to deploy on Netlify

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const COURSES_URL = "http://localhost:4000/api/courses";
-const MODULES_URL = "http://localhost:4000/api/modules";
+// const MODULES_URL = "http://localhost:4000/api/modules";
+
+const MODULES_URL =
+  "https://kanbas-node-server-app-0qk7.onrender.com/api/modules";
 
 export const updateModule = async (module) => {
   const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
